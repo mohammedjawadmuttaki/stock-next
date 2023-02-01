da3631e5e70b8191b169bc68c10227f815936044
mohammedjawadmuttaki <u6118324@au.edu>
2023-02-01 15:54:21 +0700

    Added new page

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,12 @@ import Head from 'next/head'
 import Link from 'next/link'
 
 
+
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>Jawad</title>
+    <Head> 
+      <title>Jawad </title>
     </Head>
     <h1>Jawad Page</h1>
     <p>
