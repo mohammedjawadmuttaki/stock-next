d1f115298b96d56c911a5141fc9b7fbaa26e34b4
mohammedjawadmuttaki <u6118324@au.edu>
2023-02-22 19:23:26 +0700

    stocknproduct

--- pages/api/blogs/articles/[id].js
+++ pages/api/blogs/articles/[id].js
@@ -1,20 +1,27 @@
 import { connect, model, models, Schema } from "mongoose"
 //const connectionString = 'mongodb+srv://user1:z6118324@cluster0.a9a3ze2.mongodb.net/blogs'
 const connectionString = process.env.MONGODB_URI
+
 export default async function handler(req, res) {
     await connect(connectionString);
     console.log("req.method: ", req.method)
+    console.log("req.query.id", req.query.id)
 
+    const id = req.query.id
     if (req.method === 'GET') {
-        const docs = await Article.find()
-        res.status(200).json(docs)
-    } else if (req.method === 'POST') {
-        console.log(typeof(req.body))
-        // res.status(200).json(req.body)
-        const doc = await Article.create(req.body)
-        res.status(201).json(doc)
+        // Get only one document
+        const doc = await Article.findOne({ _id: id })
+        res.status(200).json(doc)
+    } else if (req.method === 'DELETE') {
+        const deletedDoc = await Article.deleteOne({ _id: id })
+        res.status(200).json(deletedDoc)
+    } else if (req.method === 'PUT') {
+        console.log('id',req.query.id)
+        console.log(req.body)
+        const updatedDoc = await Article.updateOne({_id: id}, req.body)
+        res.status(200).json(updatedDoc)
     } else {
-        res.setHeader('Allow', ['GET', 'POST'])
+        res.setHeader('Allow', ['GET', 'DELETE'])
         res.status(405).end(`Method ${req.method} Not Allowed`)
     }
 }
